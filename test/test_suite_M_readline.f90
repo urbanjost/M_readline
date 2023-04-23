320f0d924fc5ca473a3ac037b12b2f657af96664
John S. Urban <urbanjost@comcast.net>
2023-04-23 18:04:08 -0400

    testing

--- test/test_suite_M_readline.f90
+++ test/test_suite_M_readline.f90
@@ -9,7 +9,6 @@ contains
 !TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
 subroutine test_suite_M_readline()
 use M_framework__verify, only : unit_check_start,unit_check,unit_check_done,unit_check_good,unit_check_bad,unit_check_msg
-use M_framework__verify, only : unit_check_level
 
 !! setup
    call test_system_readline()
@@ -28,12 +27,8 @@ end module M_testsuite_M_readline
 program runtest
 use M_framework__msg
 use M_framework__verify, only : unit_check, unit_check_start, unit_check_good, unit_check_bad, unit_check_done
-use M_framework__verify, only : unit_check_level, unit_check_keep_going, unit_check_command
 use M_framework__verify, only : unit_check_stop
 use M_testsuite_M_readline
-   unit_check_command=''
-   unit_check_keep_going=.true.
-   unit_check_level=0
    call test_suite_M_readline()
    call unit_check_stop()
 end program runtest
